7f14994e025a74127856db06c97dbb8834b7e59c
hartjoa <hartjoa@gmail.com>
2025-07-02 20:25:06 +0200

    Removed useless comments

--- Eliuds_eggs.py
+++ Eliuds_eggs.py
@@ -7,13 +7,3 @@ def egg_count(display_value):
         remaining -= display_value % (2 ** (i + 1))
         i += 1
     return result
-
-
-
-print(egg_count(89))           # 4
-print(egg_count(16))           # 1
-print(egg_count(0))            # 0
-print(egg_count(2000000000))   # 13   
-
-
-#89 : 01011001
